f2a5e543b05d2fdb5e07297ecf6e9f2ad2c34cb2
lighthart <dlighthart@mesd.k12.or.us>
2014-09-23 10:49:12 -0700

    Actions buttons hide/show finished

--- Resources/public/js/xtraActions.js
+++ Resources/public/js/xtraActions.js
@@ -1,27 +1,29 @@
-function gridExtraActionsControl(){
-    console.log('Xtra Actions!');
+function gridExtraActionsControl() {
     $('.lg-xtra-actions').on('click', function(e) {
         e.preventDefault();
-        console.log($(this));
-        console.log($(this).siblings());
-        console.log('siblings:');
-        console.log($(this).siblings('.lg-xtra-action'));
-        console.log('hidden siblings');
-        console.log($(this).siblings('lg-xtra-action hide'));
-        console.log($(this).siblings('lg-xtra-action hide').length);
         // this needs to be changed to find the relevant children
-        if ($(this).siblings('lg-xtra-action hide')) {
-            // Open
-            console.log('open');
-            $(this).children('span').removeClass('fa-caret-right');
-            $(this).children('span').addClass('fa-caret-left');
-            $(this).siblings('.lg-xtra-action').each(function() {console.log($(this));$(this).removeClass('hide');});
+        if ($(this).siblings('.lg-xtra-action.hide').length) {
+            showHiddenActions($(this));
         } else {
-            // Close
-            $(this).children('span').addClass('fa-caret-right');
-            $(this).children('span').removeClass('fa-caret-left');
-            console.log('close');
-            $(this).siblings('.lg-xtra-action').each(function() {$(this).addClass('hide');});
+            hideVisibleActions($(this));
         }
+    });
+}
+
+function showHiddenActions(control) {
+    // Open
+    control.children('span').removeClass('fa-caret-right');
+    control.children('span').addClass('fa-caret-left');
+    control.siblings('.lg-xtra-action').each(function() {
+        $(this).removeClass('hide');
+    });
+}
+
+function hideVisibleActions(control) {
+    // Close
+    control.children('span').addClass('fa-caret-right');
+    control.children('span').removeClass('fa-caret-left');
+    control.siblings('.lg-xtra-action').each(function() {
+        $(this).addClass('hide');
     });
 }
