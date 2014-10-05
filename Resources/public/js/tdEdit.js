7a27ea31e447da3a9484ba6de827b9aaf4fb035c
Lighthart <lighthart.coder@gmail.com>
2014-10-05 13:29:25 -0700

    Updating regex

--- Resources/public/js/tdEdit.js
+++ Resources/public/js/tdEdit.js
@@ -16,7 +16,7 @@ function makeClickable(object) {
 
 function makeEditable(object) {
     // this function loads the input field and associated controls
-
+    console.log('begin Make Editable');
     var original = object.text().trim();
     object.off('click');
     input=object.children('input');
@@ -107,6 +107,7 @@ function makeEditable(object) {
             object.children('input').focus();
         }
     });
+   console.log('end Make Editable');
 }
 
 function update(object, original, val) {
@@ -119,6 +120,7 @@ function update(object, original, val) {
         // This over writes the input field
         object.text(val);
         url = makeURLfromTD(object, 'update');
+        console.log('update else');
         console.log(url);
         $.ajax({
             type: 'POST',
@@ -127,7 +129,9 @@ function update(object, original, val) {
                 data: val
             },
             success: function(responseText, textStatus, XMLHttpRequest) {
+                console.log('update success');
                 url = makeURLfromTD(object, 'value');
+                console.log('update post makeURL');
                 console.log(url);
                 console.log(object);
 
@@ -141,6 +145,7 @@ function update(object, original, val) {
 }
 
 function makeURLfromTD(td, action) {
+    console.log('Make URL: '+ action);
     var th = td.closest('table').find('th').eq(td.index());
     var thid = th.attr('data-role-lg-parent-entity-id');
     // td must have data-role-entity-id
@@ -153,25 +158,31 @@ function makeURLfromTD(td, action) {
     var tr = td.closest('tr');
     var trid = tr.attr('data-role-lg-parent-entity-id');
     var tdid = td.attr('data-role-lg-entity-id');
+    console.log('tdid: '+tdid);
+    console.log('trid: '+trid);
+    console.log('thid: '+thid);
     if (action == 'update' && td.attr('data-role-lg-update')) {
+        console.log('datarole-update');
         url = td.attr('data-role-lg-update').replace('~entity_id~', td.attr('data-role-lg-entity-id')).replace('~col_id~', thid).replace('~row_id~', trid);
         console.log('update make url');
         console.log(url);
-        return url;
-    }
-    if (action == 'new' && td.attr('data-role-lg-new')) {
+    } else if (action == 'new' && td.attr('data-role-lg-new')) {
         url = td.attr('data-role-lg-new').replace('~entity_id~', td.attr('data-role-lg-entity-id')).replace('~col_id~', thid).replace('~row_id~', trid);
-        return url;
-    }
-    // otherwise try to figure it out yourself
-    if (typeof tdid != 'undefined') {
-        console.log('no tdid');
+    } else if (typeof tdid != 'undefined') {
+     // otherwise try to figure it out yourself
+        console.log('tdid not undefined');
         url = getLgAppRoot() + 'cell/' + action + '/' + th.attr('data-role-lg-class') + '/' + th.attr('data-role-lg-field') + '/' + tdid;
         console.log(url);
     } else {
         console.log('no trid');
         url = getLgAppRoot() + 'cell/' + action + '/' + th.attr('data-role-lg-class') + '/' + th.attr('data-role-lg-field') + '/' + trid;
         console.log(url);
+    }
+
+    if (url.indexOf('___') != -1){
+        url = url.split('___');
+        url[0]=url[0].substr(0,url[0].lastIndexOf('/')+1);
+        url=url.join('');
     }
     return url;
 }
