bc9f301d452830f95be3691bb21c8e1d1b6d2f19
lighthart <dlighthart@mesd.k12.or.us>
2015-04-24 15:53:09 -0700

    Array_Agg backadded

---
+++ DQL/Postgres/ArrayAgg.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Lighthart\GridBundle\DQL\Postgres;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+/**
+ *
+ */
+class ArrayAgg extends FunctionNode
+{
+    public $field;
+    /**
+     *  @override
+     */
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return "array_agg(" . $sqlWalker->walkArithmeticPrimary($this->field) . ")";
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->field = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
