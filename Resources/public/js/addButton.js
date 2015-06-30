adcda877aa389f1bc45460ae88002e82c1c92a9a
lighthart <dlighthart@mesd.k12.or.us>
2015-06-30 10:37:44 -0700

    Add form under opener now

--- Resources/public/js/addButton.js
+++ Resources/public/js/addButton.js
@@ -1,21 +1,21 @@
 function addButtonToggleControl() {
-    $('#lg-toggle-add-form').unbind('click');
-    $('#lg-toggle-add-form').one('click', function(e) {
+    $('#lg-add-form-toggle').unbind('click');
+    $('#lg-add-form-toggle').one('click', function(e) {
         e.preventDefault();
         var addFormToggleCookie = 'lg-add-form-toggle';
         if ($('#lg-add-form').hasClass('hide')) {
             // Open
             $.cookie(addFormToggleCookie, 1);
-            $('#lg-add-icon').removeClass('fa-chevron-right');
-            $('#lg-add-icon').addClass('fa-chevron-left');
+            $('#lg-add-icon').removeClass('fa-chevron-down');
+            $('#lg-add-icon').addClass('fa-chevron-up');
             $('#lg-add-form').removeClass('hide');
             $('#lg-add-form-toggle').removeClass('add-form-closed');
             $('#lg-add-form-toggle').addClass('add-form-open');
         } else {
             // Closed
             $.cookie(addFormToggleCookie, 0);
-            $('#lg-add-icon').addClass('fa-chevron-right');
-            $('#lg-add-icon').removeClass('fa-chevron-left');
+            $('#lg-add-icon').addClass('fa-chevron-down');
+            $('#lg-add-icon').removeClass('fa-chevron-up');
             $('#lg-add-form').addClass('hide');
             $('#lg-add-form-toggle').addClass('add-form-closed');
             $('#lg-add-form-toggle').removeClass('add-form-open');
