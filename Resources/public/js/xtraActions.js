37cb86644162eab89ad1024c99b78730c714bf47
lighthart <dlighthart@mesd.k12.or.us>
2016-08-01 11:32:22 -0700

    Fixed newline xtraaction close

--- Resources/public/js/xtraActions.js
+++ Resources/public/js/xtraActions.js
@@ -1,32 +1,50 @@
 // '@LighthartGridBundle/Resources/public/js/xtraActions.js'
 function gridExtraActionsControl() {
-    $('.lg-xtra-actions').on('click', function(e) {
-        e.preventDefault();
-        // this needs to be changed to find the relevant children
-        if ($(this).siblings('.lg-xtra-action.hide').length) {
-            showHiddenActions($(this));
-        } else {
-            hideVisibleActions($(this));
-        }
-    });
+    $('.lg-xtra-actions')
+        .on('click', function(e) {
+            e.preventDefault();
+            // this needs to be changed to find the relevant children
+            if ($(this)
+                .siblings('.lg-xtra-action.hide')
+                .length) {
+                showHiddenActions($(this));
+            }
+            else {
+                hideVisibleActions($(this));
+            }
+        });
 }
 
 function showHiddenActions(control) {
     // Open
-    control.children('span').removeClass('fa-caret-right');
-    control.children('span').addClass('fa-caret-left');
-    control.siblings('.lg-xtra-action').each(function() {
-        $(this).removeClass('hide');
-    });
-    control.parent().attr('style', 'width:' + control.parent().children().length * 24 + 'px;');
+    control.children('span')
+        .removeClass('fa-caret-right');
+    control.children('span')
+        .addClass('fa-caret-left');
+    control.siblings('.lg-xtra-action')
+        .each(function() {
+            $(this)
+                .removeClass('hide');
+        });
+    control.parent()
+        .attr('style', 'width:' + control.parent()
+            .children()
+            .length * 24 + 'px;');
 }
 
 function hideVisibleActions(control) {
     // Close
-    control.children('span').addClass('fa-caret-right');
-    control.children('span').removeClass('fa-caret-left');
-    control.siblings('.lg-xtra-action').each(function() {
-        $(this).addClass('hide');
-    });
-    control.parent().attr('style', 'width:' + 4 * 24 + 'px;');
+    control.children('span')
+        .addClass('fa-caret-right');
+    control.children('span')
+        .removeClass('fa-caret-left');
+    control.siblings('.lg-xtra-action')
+        .each(function() {
+            $(this)
+                .addClass('hide');
+        });
+    // This is related to padding set in cell.html.twig. 
+    // Bad coupling here
+    control.parent()
+        .attr('style', 'width:' + 6 * 24 + 'px;');
 }
